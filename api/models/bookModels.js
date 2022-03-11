ad0bb7ef5a0c0d9bbbfff4db5cdd7b358cfcace8
Sarawut Bunmee <owutnr@gmail.com>
2022-03-11 22:32:47 +0700

    update bookModels

--- api/models/bookModels.js
+++ api/models/bookModels.js
@@ -37,7 +37,7 @@ const bookSchema = new Schema(
         },
         img: {
             type: String,
-            required: true
+            required: false
           },
     },
     { collection: 'books' }
